5ff577ea1f639b3f452ab7005cdc773bd2f4ddec
Stequila93 <62798379+Stequila93@users.noreply.github.com>
2021-12-10 17:57:05 +0100

    Update lcd_i2c.h

--- lcd_i2c.h
+++ lcd_i2c.h
@@ -5,7 +5,7 @@ I2C_HandleTypeDef lcdi2c;
 void lcd_init(void);
 void lcd_clear(void);
 void lcd_print(char *stringa);
-void lcd_clearString(uint8_t col, uint8_t col_b, uint8_t row);
+void lcd_clearString(uint8_t col_a, uint8_t col_b, uint8_t row);
 void lcd_setCursor(uint8_t col, uint8_t row);
 void lcd_sendCommand(uint8_t command);
 void lcd_printChar(char character);
